98298d70e2b337b70c58a518d07224611b2fb82f
Kevin Xu <kevin.xu@asb.co.nz>
2015-08-06 15:46:39 +1200

    init commit

---
+++ app/init.ts
@@ -0,0 +1,8 @@
+System.config({
+  baseURL: '<%= APP_BASE %>',
+  paths: {'*': '*.js?v=<%= VERSION %>'}
+});
+
+System.import('app')
+  .catch(e => console.error(e,
+    'Report this error at https://github.com/mgechev/angular2-seed/issues'));
